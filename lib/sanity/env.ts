36f68318103f06adea45df2c557cab01cfc57f0d
Lab Team <lab@example.com>
2026-02-11 14:01:38 +0900

    fix: hardcode sanity project id for environment variable fallback

--- lib/sanity/env.ts
+++ lib/sanity/env.ts
@@ -4,6 +4,6 @@ export const apiVersion =
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 
 // Use a valid but dummy placeholder to pass build validation if env var is missing
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dummy-project';
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '73iofnnm';
 
 export const useCdn = false
